472c503c6a47f037f769a70f2a67977229b13b11
Wouter Avet <wouter.avet@telenet.be>
2017-08-26 15:31:01 +0000

    Added upload confirmation after input form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,22 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
+        else
+            flash[:error] = 'Error occured, message has not been sent.'
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        # Dit stuk code zal de inkomende gegevens van de contact-from ontvangen en in een array aanbieden aan contact.new om er een nieuw contact mee te maken
+        # Dit zal een een privaat stuk gebeuren = achter gesloten deuren in de code
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
+    
 end
